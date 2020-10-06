31bb726b4193274c5bf69811268a0bfb34efe5b5
ryandedelstein <ryan.d.edelstein@vanderbilt.edu>
2020-10-06 18:13:00 -0500

    Optim

--- OptimXMartin.js
+++ OptimXMartin.js
@@ -3,12 +3,6 @@ var config = {
     xAimed : { value: 1.088536, type: 'multiplier', label: 'Multiplier to aim' }
 }
 
-//Reverse martingale by @Cannonball
-//Feel free to tip, as it is a free script
-//Also feel free to ping me if you got questions
-//Idea is to bet more after you win, and return to base bet when lost
-//After x win, return to base bet to save the profit
-
 var currentWinStreak = 0;
 var userProfit = 0;
 var currentBet = config.baseBet.value;
